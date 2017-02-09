09ed1969b25ec6856e8dd9358af9a4427809863f
Sam Bunting <sambunting2008@live.co.uk>
2017-02-09 21:18:10 +0000

    Added style

--- index.php
+++ index.php
@@ -2,35 +2,41 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>Document</title>
+		<title>RSS Reader</title>
+		<link rel="stylesheet" type="text/css" href="style/stylesheet.css">
 	</head>
 	<body>
-		<?php 
-			function get_feed($url) {
-				$content = file_get_contents($url);
-				$xml = new SimpleXmlElement($content);
+		<h1>RSS Reader</h1>
 
-				echo '<h2>'.$xml->channel->title.'</h2>';
-				echo '<img src="'.$xml->channel->image->url.'"></img>';
+		<main>
+			<?php 
+				function get_feed($url) {
+					$content = file_get_contents($url);
+					$xml = new SimpleXmlElement($content);
+					
+					echo '<img id="feed-image" src="'.$xml->channel->image->url.'"></img>';
+					echo '<h2 id="feed-title">'.$xml->channel->title.'</h2>';
+					
 
-				echo '<ul>';
-
-				foreach ($xml->channel->item as $entry) {
-					echo '<li><a href="'.$entry->link.'">'.$entry->title.'</a> - <i>'.$entry->pubDate.'</i></li>';
+					foreach ($xml->channel->item as $entry) {
+						echo '<a href="'.$entry->link.'"><div class="rss-item">';
+						echo $entry->title.'<br><i>'.$entry->pubDate;
+						echo '</i></div></a>';
+					}
 				}
 
-				echo '</ul>';
-			}
+				if (isset($_GET['rss_url'])) {
+					get_feed($_GET['rss_url']);
+				} else {
+					?>
+						<form>
+							<input type="text" name="rss_url" id="rss_url">
+						</form>
+					<?php
+				}
+			?>
+		</main>
 
-			if (isset($_GET['rss_url'])) {
-				get_feed($_GET['rss_url']);
-			} else {
-				?>
-					<form>
-						<input type="text" name="rss_url" id="rss_url">
-					</form>
-				<?php
-			}
-		?>
+		
 	</body>
 </html>
